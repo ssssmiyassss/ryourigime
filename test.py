b521338390336a6250dc455dec458579ece8ead0
ssssmiyassss <seigo.miyamoto@gmail.com>
2021-07-19 10:52:32 +0900

    test.py とりあえずランダムに2つの料理リストからメニューを引っ張って表示する

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import random
+
 # 2021-07-18 19:17:04
 # 文字の分割＆return それぞれの要素が入ったリスト。
 def moji_split(zenbun_str):
@@ -7,6 +9,28 @@ def moji_split(zenbun_str):
   return moji_list
 
 
-zenbun_str = '野菜,根菜のラタトゥイユ,24'
-print(zenbun_str)
-moji_list = moji_split(zenbun_str)
+# 2021-07-19 10:14:19
+# main
+# zenbun_str = '野菜,根菜のラタトゥイユ,24'
+# print(zenbun_str)
+# moji_list = moji_split(zenbun_str)
+
+# file_bistro = open('bistro.txt','r')
+# file_staub  = open('staub.txt' ,'r')
+
+with open("bistro.txt",'r',encoding="utf-8_sig") as file_bistro:
+  list_bistro = file_bistro.readlines()
+# print("len(list_bistro)="+str(len(list_bistro)))
+
+with open("staub.txt" ,'r',encoding="utf-8_sig") as file_staub:
+  list_staub  = file_staub.readlines()
+# print("len(list_staub)="+str(len(list_staub)))
+
+rand_bistro = random.randint(0,len(list_bistro)-1)
+rand_staub  = random.randint(0,len(list_staub) -1)
+
+print("ビストロ＝"+str(list_bistro[rand_bistro]))
+print("ストウブ＝"+str(list_staub[ rand_staub] ))
+
+
+
